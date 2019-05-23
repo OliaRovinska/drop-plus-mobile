aba48eb3d7cbbe349cfb99901a3512de6b01120e
OlesKorzhynskiy <oles.korzhynskiy@gmail.com>
2019-05-24 00:25:01 +0300

    Added ResortListView control and connect it to Resorts page

---
+++ DropPlus/DropPlus/DropPlus/Controls/ResortListView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DropPlus.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DropPlus.Controls
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ResortListView : ContentView
+	{
+        public static readonly BindableProperty ResortsProperty = BindableProperty.Create(
+            propertyName: "Resorts",
+            returnType: typeof(List<ResortModel>),
+            declaringType: typeof(ResortListView));
+
+        public List<ResortModel> Resorts
+        {
+            get => (List<ResortModel>)GetValue(ResortsProperty);
+            set => SetValue(ResortsProperty, value);
+        }
+
+        public ResortListView ()
+		{
+			InitializeComponent ();
+        }
+    }
+}
--- DropPlus/DropPlus/DropPlus/ViewModels/ResortsViewModel.cs
+++ DropPlus/DropPlus/DropPlus/ViewModels/ResortsViewModel.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using DropPlus.Models;
 
 namespace DropPlus.ViewModels
 {
     public class ResortsViewModel : BaseViewModel
     {
+        public List<ResortModel> Resorts { get; set; }
+
         public ResortsViewModel()
         {
+            Resorts = new List<ResortModel>()
+            {
+                new ResortModel()
+                {
+                    Image = "https://upload.wikimedia.org/wikipedia/commons/d/df/Town_and_Country_fh000023.jpg",
+                    Location = "Солотвино"
+                },
+                new ResortModel()
+                {
+                    Image = "https://media-cdn.tripadvisor.com/media/photo-s/08/4a/79/34/pushkar-resorts.jpg",
+                    Location = "Синевир"
+                }
+            };
         }
     }
 }
