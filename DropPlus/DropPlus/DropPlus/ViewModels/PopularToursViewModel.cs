ea973f38e35465e0660ea8a5104a7a4250808a95
OlesKorzhynskiy <oles.korzhynskiy@gmail.com>
2019-05-23 22:54:08 +0300

    Initial commit

---
+++ DropPlus/DropPlus/DropPlus/ViewModels/PopularToursViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DropPlus.ViewModels
+{
+    public class PopularToursViewModel : BaseViewModel
+    {
+        public PopularToursViewModel()
+        {
+        }
+    }
+}
