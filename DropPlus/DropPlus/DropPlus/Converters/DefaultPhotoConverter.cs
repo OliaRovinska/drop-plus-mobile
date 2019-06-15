c46febaf9c556e52134b9f2ccb7e2889ce1e958c
OlesKorzhynskiy <oles.korzhynskiy@gmail.com>
2019-06-15 23:51:57 +0300

    Added default photo converter

---
+++ DropPlus/DropPlus/DropPlus/Converters/DefaultPhotoConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace DropPlus.Converters
+{
+    public class DefaultPhotoConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (ImageSource)value != null ? value : "default_photo.jpg";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
